9ee3abbf1035be5dbeeed01ef73f23179a936bda
agarofi <garone80@gmail.com>
2017-05-02 18:31:09 +0300

    sun page, tdd on mercury

---
+++ js/sun.js
@@ -0,0 +1,132 @@
+/* ephemeris - a software astronomical almanach 
+
+Copyright 2017 Herr_Alien <alexandru.garofide@gmail.com>
+
+This program is free software: you can redistribute it and/or modify it under 
+the terms of the GNU Affero General Public License as published by the 
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/agpl.html>. */
+
+
+(function(){    
+
+    var SunData = {
+        cache : {},
+        getDataForJD : function (JD) {
+            var line = this.cache[JD];
+            if (!line) {
+                line = [];
+                
+                var _date = AAJS.Date.JD2Date(JD);
+                // convert from JD to gregorian
+                line[0] = _date.M;
+                line[1] = _date.D;
+                var radec = AAJS.Sun.EquatorialCoordinates(JD, true);
+                line[2] = radec.X; // RA [h.hhhh]
+                line[3] = radec.Y; // DEC [deg.dddd]
+                line[4] = AAJS.Sun.Diameter(JD, true)/3600; // [deg.dddd]
+                // transit should be computed from the RA (LST to UTC conversion)
+                var jdOfTransit = AAJS.Date.ST2NextJD(radec.X, JD);
+                radec = AAJS.Sun.EquatorialCoordinates(jdOfTransit - 5 /(24 * 60), true);
+                jdOfTransit = AAJS.Date.ST2NextJD(radec.X, jdOfTransit);
+                var transitHour = 24 * (jdOfTransit - JD);
+                line[5] = transitHour;
+                var physical = AAJS.Sun.CalculatePhysicalDetails(JD, true);
+                line[6] = physical.P; // [deg.dddd]
+                line[7] = physical.B0; // [deg.dddd]
+                line[8] = physical.L0; // [deg.dddd]
+            }
+            this.cache[JD] = line;
+            return line;
+        },
+        initFromLocalStorage : function () {
+            // TODO: this is where we fetch data already computed during earlier sessions
+        }
+    };
+    SunData.initFromLocalStorage();
+
+    var SunPage = {
+        page : document.getElementById("SunPage"),
+        table : document.getElementById("Sun"),
+
+        reset : function () {
+            while (this.table.hasChildNodes()) {
+                var currentTr = this.table.lastElementChild;
+                if (currentTr.className == "fixed") // not the safest way
+                    break;
+                this.table.removeChild(currentTr);
+            }
+        },
+        
+        prepareLineForView : function (line) {
+            var displayableLine = [];
+            // copy the day verbatim
+            displayableLine[1] = line[1];
+            if (line[1] == 1) { // first day of the month
+                var months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+                displayableLine[0] = months[line[0]]; // set displayableLine[0] to the name of the month
+            }
+            else
+                displayableLine[0] = "";
+            
+            var di = 2;
+            var si = 2;
+            var sexagesimalRA = AAJS.Numerical.ToSexagesimal(line[si++]);
+            displayableLine[di++] = sexagesimalRA.Ord3 ;
+            displayableLine[di++] = sexagesimalRA.Ord2 
+            displayableLine[di++] = sexagesimalRA.Ord1;
+
+            var sexagesimalDec = AAJS.Numerical.ToSexagesimal(line[si++]);
+            displayableLine[di++] = sexagesimalDec.Ord3 ;
+            displayableLine[di++] = sexagesimalDec.Ord2;
+            displayableLine[di++] = sexagesimalDec.Ord1;
+            
+            var sexagesimalDiam = AAJS.Numerical.ToSexagesimal(line[si++]);
+            displayableLine[di++] = sexagesimalDiam.Ord2;
+            displayableLine[di++] = sexagesimalDiam.Ord1;
+            
+            var sexagesimalTransit = AAJS.Numerical.ToSexagesimal(line[si++]);
+            displayableLine[di++] = sexagesimalTransit.Ord3;
+            displayableLine[di++] = sexagesimalTransit.Ord2;
+            displayableLine[di++] = sexagesimalTransit.Ord1;
+            
+            displayableLine[di++] = AAJS.Numerical.RoundTo3Decimals (line[si++]);
+            displayableLine[di++] = AAJS.Numerical.RoundTo3Decimals (line[si++]);
+            displayableLine[di++] = AAJS.Numerical.RoundTo3Decimals (line[si++]);
+
+            return displayableLine;
+        },
+        
+        // this will probably become an utility available for every page
+        appendLine : function (dataArray) {
+            var line = this.table.ownerDocument.createElement("tr");
+            var tbody = this.table.getElementsByTagName("tbody")[0];
+            tbody.appendChild(line);
+            
+            var i = 0;
+            for (i = 0; i < dataArray.length; i++) {
+                var td = line.ownerDocument.createElement("td");
+                line.appendChild(td);
+                td.textContent = dataArray[i];
+            }
+        },
+        
+        displaySunPage : function(JD, daysAfter) {
+            if (!AAJS.AllDependenciesLoaded())
+                return setTimeout (function() { SunPage.displyaySunPage(JD, daysAfter); }, 100);
+            var i = 0;
+            for (i = 0; i < daysAfter; i++)
+                SunPage.appendLine (SunPage.prepareLineForView(SunData.getDataForJD(JD + i)));
+        }
+    };
+
+    setTimeout( function() { SunPage.displaySunPage(AAJS.Date.DateToJD (2017, 1, 1, true), 365); }, 100);
+    
+})();
---
+++ testlib.js
@@ -0,0 +1,37 @@
+var testlib = testlib || {
+    
+        logTest: function  (test, passed)
+        {
+            var paragraph = document.createElement("p");
+           if (passed)
+           {
+                paragraph.style.color = "green";
+                paragraph.innerHTML = test.name + " passed.";
+            }
+            else
+             {
+                paragraph.style.color = "red";
+                paragraph.innerHTML = test.name + " failed. Expected " + JSON.stringify(test.expectedOutput) + ", got instead " + JSON.stringify(test.actualOutput);
+            }
+            document.body.appendChild (paragraph);
+        },
+        
+        doubleEq: function  (a, b, eps)
+        {
+            return Math.abs(a - b) < eps;
+        },
+        
+        runTests : function (tests)
+        {
+            var i = 0; 
+            for (i = 0; i < tests.length; i++)
+            {
+                var currentTest = tests[i];
+                var testPassed = false;
+                try { 
+                testPassed = currentTest.test(); 
+                } catch (e){currentTest.actualOutput += " : " + e.message;}
+                testlib.logTest(currentTest, testPassed);
+            }
+        }
+};
