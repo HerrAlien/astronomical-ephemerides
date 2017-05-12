062e45046bb040416d658e846669d83a66575bee
Herr_Alien <garone80@gmail.com>
2017-05-12 10:02:07 +0200

    prep up the test after the UI, data after the test

--- js/mercury.js
+++ js/mercury.js
@@ -19,19 +19,27 @@ var MercuryData = {
     getDataForJD : function (JD) {
         var data = [];
         
+		var i = 0;
         var dateOfJD =  AAJS.Date.JD2Date(JD);
-        data[0] = dateOfJD.M;
-        data[1] = dateOfJD.D;
+        data[i++] = dateOfJD.M;
+        data[i++] = dateOfJD.D;
         
         var planetaryDetails = AAJS.Elliptical.CalculatePlanetaryDetails (JD, 1, true);
         
         //!! These are fairly low precision, need to investigate why ...
-        data[2] = planetaryDetails.ApparentGeocentricRA;
-        data[3] = planetaryDetails.ApparentGeocentricDeclination;
+        data[i++] = planetaryDetails.ApparentGeocentricRA;
+        data[i++] = planetaryDetails.ApparentGeocentricDeclination;
         
         var delta = planetaryDetails.ApparentGeocentricDistance;
         
-        data[4] = 2 * AAJS.Diameters.MercurySemidiameterB(delta) / 3600;
+        data[i++] = 2 * AAJS.Diameters.MercurySemidiameterB(delta) / 3600;
+		
+		var jdOfTransit = AAJS.Date.ST2NextJD(planetaryDetails.ApparentGeocentricRA, JD);
+//		planetaryDetails = AAJS.Elliptical.CalculatePlanetaryDetails (jdOfTransit - 6.0 /24, 1, true);
+//		jdOfTransit = AAJS.Date.ST2NextJD(planetaryDetails.ApparentGeocentricRA, jdOfTransit);
+		var transitHour = 24 * (jdOfTransit - JD);
+		data[i++] = transitHour;
+		data[i++] = delta;
 		
 		var sunEarthDistance = SunData.getSunEarthDistance(JD);
         
@@ -51,18 +59,21 @@ var MercuryData = {
 		var eccentricAnomaly = AAJS.Elliptical.EccentricAnomalyFromMeanAnomaly(meanAnomaly, eccentricity);
 		var a = AAJS.ElementsPlanetaryOrbit.MercurySemimajorAxis(JD);
 		var r =  a *  (1 -  eccentricity * Math.cos(eccentricAnomaly) );
+		data[i++] = r;
+		
 		/*
 			sunEarthDistance**2 = r**2 + delta **2 - 2 * delta * r * cos (phase);
 			2 delta r cos phase = r **2 + delta **2 - sunEarthDistance**2
 			phase = acos (( r **2 + delta **2 - sunEarthDistance**2) / (2 * delta * r))
 		*/
         
-        var cosPhaseAngle = (r*r + delta * delta - sunEarthDistance * sunEarthDistance)/(2 * delta * r);
         // debug ...
         // data[5] = 0.5 * (cosPhaseAngle + 1);
 		
 		var cosElongationAngle = (delta * delta + sunEarthDistance * sunEarthDistance - r * r)/(2 * delta * sunEarthDistance);
-		data[5] = Math.acos(cosElongationAngle);
+		data[i++] = Math.acos(cosElongationAngle);
+        var cosPhaseAngle = (r*r + delta * delta - sunEarthDistance * sunEarthDistance)/(2 * delta * r);
+		data[i++] = 0.5 * (cosPhaseAngle + 1);
         return data;
     }
 };
