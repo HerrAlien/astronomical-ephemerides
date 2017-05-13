aeee1982bcb3a2e6ce474c14b007a2af7b5fad15
Herr_Alien <garone80@gmail.com>
2017-05-13 21:09:36 +0300

    jupiter test ...

--- js/jupiter.js
+++ js/jupiter.js
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Affero General Public License along
 with this program. If not, see <https://www.gnu.org/licenses/agpl.html>. */
 
 var JupiterData = new PlanetData({ number: 4, name: "Jupiter", 
-                               semidiameterFunctionName : AAJS.Diameters.MarsSemidiameterB });
+                               semidiameterFunctionName : AAJS.Diameters.JupiterEquatorialSemidiameterB });
 
 							   
 (function () {
