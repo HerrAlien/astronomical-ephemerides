ee12c1c20e97f61eda7fb252aca15e5bd188ba12
agarofi <garone80@gmail.com>
2017-05-30 09:46:06 +0300

    physical details for Jupiter

--- js/jupiter.js
+++ js/jupiter.js
@@ -17,10 +17,57 @@ with this program. If not, see <https://www.gnu.org/licenses/agpl.html>. */
 var JupiterData = new PlanetData({ number: 4, name: "Jupiter", 
                                semidiameterFunctionName : AAJS.Diameters.JupiterEquatorialSemidiameterB });
 
+// upgrade the object to handle physical data as well.
+(function () {    
+    JupiterData['old_GetData'] = JupiterData.getDataForJD;
+    JupiterData['physicalDataCache'] = {};     
+    JupiterData.getDataForJD = function (JD) {
+        var data = this.old_GetData(JD);            
+        var physicalData =  this.physicalDataCache[JD];
+        if (!physicalData) {
+            physicalData = AAJS['Jupiter']['PhysicalDetails'] (JD);
+            this.physicalDataCache[JD] = physicalData;
+        }
+        data[data.length]  = physicalData.CentralMeridianApparentLongitude_System1;
+        data[data.length]  = physicalData.CentralMeridianApparentLongitude_System2;
+        data[data.length]  = physicalData.EarthDeclination;
+        data[data.length]  = physicalData.SunDeclination;
+        data[data.length]  = physicalData.P;
+        return data;
+    };
+})();
 							   
 (function () {
     var Page = new PlanetPage (JupiterData);
         Pages["JupiterPage"] = Page;
 
+    Page.tableHeaderInfo['16'] = { "0" : "L0-S1", "1" : "\u00B0", "longText" : "Longitude of central meridian, System 1" };
+    Page.tableHeaderInfo['17'] = { "0" : "L0-S2", "1" : "\u00B0", "longText" : "Longitude of central meridian, System 2" };
+    Page.tableHeaderInfo['18'] = { "0" : "DE", "1" : "\u00B0", "longText" : "Planetocentric declination of Earth" };
+    Page.tableHeaderInfo['19'] = { "0" : "DS", "1" : "\u00B0", "longText" : "Planetocentric declination of the Sun" };
+    Page.tableHeaderInfo['20'] = { "0" : "P", "1" : "\u00B0", "longText" : "Position angle of the North Pole" };
+
+    Page["old_prepareLineForView"] = Page.prepareLineForView;
+    Page.prepareLineForView = function (line, JD) {
+        var preparedLine = this.old_prepareLineForView(line, JD);
+        preparedLine[preparedLine.length] = Math.round(line[10] * 10) / 10;
+        preparedLine[preparedLine.length] = Math.round(line[11] * 10) / 10;
+        preparedLine[preparedLine.length] = Math.round(line[12] * 10) / 10;
+        preparedLine[preparedLine.length] = Math.round(line[13] * 10) / 10;
+        preparedLine[preparedLine.length] = Math.round(line[14] * 10) / 10;
+        return preparedLine;
+    }
+
+    Page["old_addPlanetTableHeader"] = Page.addPlanetTableHeader;
+    Page.addPlanetTableHeader = function (table, classes) {
+        var headerRows = this.old_addPlanetTableHeader(table, classes);
+        var cellL0S1 = headerRows.row1.cells[16];
+        cellL0S1.textContent = "L";
+        this.addNodeChild (cellL0S1, "sub", "0,S1"); 
+        var cellL0S2 = headerRows.row1.cells[17];
+        cellL0S2.textContent = "L";
+        this.addNodeChild (cellL0S2, "sub", "0,S2"); 
+        return headerRows;
+    }
 
 })();
