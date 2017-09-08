b4160dfe876ba06176a080a6baafe4fba7d5165f
agarofi <garone80@gmail.com>
2017-09-08 19:28:48 +0300

    reversing matrices

--- js/matrix.js
+++ js/matrix.js
@@ -68,11 +68,58 @@ var Matrix = {
         if (matrix.length != matrix[0].length)
             throw "Not a square matrix; has " + matrix.length + " rows and " + matrix[0].length + " columns!";
         
+        var tmpMatrix = [];
+        for (var i = 0; i < matrix.length; i++) {
+            tmpMatrix[i] = new Array (matrix[0].length);
+            for (var j = 0; j < matrix[0].length; j++)
+                tmpMatrix[i][j] = matrix[i][j];
+        }
+                
+        // append the unit matrix
+        Matrix.appendUnitMatrix (tmpMatrix);
+        // then for each column, from 0 to the old max. col:
+        for (var colIndex = 0; colIndex < matrix[0].length; colIndex++) {
+            // - get the row with the largest value on that column.
+            var deservingRowIndex = colIndex;
+            for (var searchingRowIndex = deservingRowIndex; searchingRowIndex < tmpMatrix.length; searchingRowIndex++) {
+                if (tmpMatrix[searchingRowIndex][colIndex] > tmpMatrix[deservingRowIndex][colIndex])
+                    deservingRowIndex = searchingRowIndex;
+            }
+            // - swap it with the row that is placed at row index = current column index
+            Matrix.swapRows(tmpMatrix, deservingRowIndex, colIndex);
+            // - accumulate it to all the other rows, to reduce that row's column position to 0
+            var scaleFactor = 1/tmpMatrix[colIndex][colIndex];
+            for (var accumulateRowIndex = 0; accumulateRowIndex < tmpMatrix.length; accumulateRowIndex++) {
+                if (accumulateRowIndex == colIndex)
+                    continue;
+                Matrix.accumulateMultipliedRow(tmpMatrix, accumulateRowIndex, colIndex, -(tmpMatrix[accumulateRowIndex][colIndex] * scaleFactor));
+            }
+            // - scale the row down, so that the position at current column index is 1.
+            Matrix.scaleRow (tmpMatrix, colIndex, scaleFactor);
+        }
         
+        var result = new Array (matrix.length);
+        for (var i = 0; i < result.length; i++)
+            result[i] = new Array(matrix.length);
         
+        for (var i = 0; i < matrix.length; i++)
+            for (var j = 0; j < matrix.length; j++)
+                result[i][j] = tmpMatrix[i][j+matrix.length];
         
-        
-        
+        return result;
     },
+    
+    areEqual : function (m1, m2, eps) {
+        if (m1.length != m2.length)
+            return false;
+        if (m1[0].length != m2[0].length)
+            return false;
+        for (var i = 0; i < m1.length; i++) {
+            for (var j = 0; j < m1[i].length; j++)
+                if (!m2[i][j] || Math.abs(m1[i][j] - m2[i][j]) > eps)
+                    return false;
+        }
+        return true;
+    }
         
 };
