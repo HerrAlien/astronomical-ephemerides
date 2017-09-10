f8513f658ee1040a372c67e05b3de02af415e65d
Herr_Alien <garone80@gmail.com>
2017-09-10 14:34:22 +0300

    moved the taylor series coeffs.

--- js/numerical.js
+++ js/numerical.js
@@ -30,5 +30,54 @@ var FunctionFitting = {
             res[i] = coeffColumn[i][0];
         
         return res;
+    },
+    
+    Taylor_ForwardDiff : function (ValuesArray) {
+        var coefficients = [];
+        if (ValuesArray.length >= 4) {
+        
+            coefficients[0] = ValuesArray[0];
+            coefficients[1] = ValuesArray[1] - ValuesArray[0];
+            coefficients[2] = ValuesArray[2] - 2 * ValuesArray[1] + ValuesArray[0];
+            coefficients[3] = ValuesArray[3] - 3 * ValuesArray[2] + 3 * ValuesArray[1] - ValuesArray[0];
+
+            coefficients[2] /= 2;
+            coefficients[3] /= 6;
+
+        }
+        
+        return coefficients;
+    },
+            
+    Taylor_BackwardDiff : function (ValuesArray) {
+        var coefficients = [];
+        if (ValuesArray.length >= 4) {
+        
+            coefficients[0] = ValuesArray[3];
+            coefficients[1] = ValuesArray[3] - ValuesArray[2];
+            coefficients[2] = ValuesArray[3] - 2 * ValuesArray[2] + ValuesArray[1];
+            coefficients[3] = ValuesArray[3] - 3 * ValuesArray[2] + 3 * ValuesArray[1] - ValuesArray[0];
+
+            coefficients[2] /= 2;
+            coefficients[3] /= 6;
+
+        }
+        
+        return coefficients;
+    },
+            
+    Taylor_Average : function (ValuesArray) {
+    
+        var forwardValues = ValuesArray.slice (3, 7);
+        var backwardValues = ValuesArray.slice (0, 4);
+    
+        var forwards = FunctionFitting.Taylor_ForwardDiff(forwardValues);
+        var backwards = FunctionFitting.Taylor_BackwardDiff (backwardValues);
+
+        return [ 0.5 * (backwards[0] + forwards[0]),
+                 0.5 * (backwards[1] + forwards[1]),
+                 0.5 * (backwards[2] + forwards[2]),
+                 0.5 * (backwards[3] + forwards[3])];
     }
+
 };
