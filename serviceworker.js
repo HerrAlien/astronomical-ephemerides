52f81a05452c4f8bdd81e2d3f19132f606c4c5c2
agarofi <garone80@gmail.com>
2018-01-29 18:19:20 +0200

    copyright, fix printing issue

--- serviceworker.js
+++ serviceworker.js
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Affero General Public License along
 with this program. If not, see <https://www.gnu.org/licenses/agpl.html>. */
 (function() {
 var CACHE_PREFIX = 'Cache-for-ephemerides';
-var CACHE_VERSION = 'v23';
+var CACHE_VERSION = 'v24';
 var CACHE_NAME = CACHE_PREFIX + '-' + CACHE_VERSION;
 var urlsToCache = [
 "/ephemerides/",
