3a5ed0409fe2599be96a335840d55d26a4d19b78
agarofi <garone80@gmail.com>
2018-02-13 17:58:24 +0200

    factor out a fetch and cache

--- serviceworker.js
+++ serviceworker.js
@@ -95,6 +95,21 @@ self.addEventListener('install', function(event) {
   );
 });
 
+function fetchAndCache (url) {
+  return fetch (url).then (resp => {
+    if(!resp || resp.status !== 200 || resp.type !== 'basic') {
+      return resp;
+    }
+
+    var responseToCache = resp.clone();
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        cache.put(url, responseToCache);
+       });
+
+    return resp;
+  });
+}
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
@@ -105,33 +120,7 @@ self.addEventListener('fetch', function(event) {
           return response;
         }
         
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
+        return fetchAndCache (event.request);
       }
     )
   );
