6f60a2f225b60680e53ac4bb56caddcdad733985
agarofi <garone80@gmail.com>
2018-02-16 18:19:40 +0200

    move to a namespace ...

--- js/realtime.js
+++ js/realtime.js
@@ -18,64 +18,70 @@ with this program. If not, see <https://www.gnu.org/licenses/agpl.html>. */
 
 
 // ---------------------------- view side ----------------------------------------    
- 
-     function RealTimeDataViewer (pageName) {
-        this.page = Pages[pageName];
+var RealTimeDataViewer = {
 
-        this.reset = function() {
+    New: function (pageName) {
 
-            var doms = CreateLinkDom (pageName);
-            this.view = doms["a"];
+        var returnedViewer = {
 
-            // views, per daya key
-            // TODO: get the keys!
-            this.allKeys = [];
+            page: Pages[pageName],
 
-            this.allViews = {};
-            for (var i = 0; i < this.allKeys.length; i++) {
-                var name = this.allKeys[i].name;
-                this.allViews[name] = this.view.getElementsByClassName(name)[0];
-            }
-            // TODO: on a settings notification, update the visibility for all entries in this.allViews
+            reset: function () {
 
-            this.rtData = new DataForNow(this.page.dataSource);
-            var obj = this;
-            this.rtData.onDataUpdated.add (function(data) {
+                var doms = RealTimeDataViewer.CreateLinkDom(pageName);
+                this.view = doms["a"];
+
+                // views, per daya key
+                // TODO: get the keys!
+                this.allKeys = [];
+
+                this.allViews = {};
+                for (var i = 0; i < this.allKeys.length; i++) {
+                    var name = this.allKeys[i].name;
+                    this.allViews[name] = this.view.getElementsByClassName(name)[0];
+                }
+                // TODO: on a settings notification, update the visibility for all entries in this.allViews
+
+                this.rtData = new DataForNow(this.page.dataSource);
+                var obj = this;
+                this.rtData.onDataUpdated.add(function (data) {
 
 
-                for (var i = 0; i < obj.allKeys.length; i++) {
-                    var key = obj.allKeys[i];
-                    var decimals = Math.pow(10, key.decimalsNum);
-                    var name = key.name;
-                    var keyData = Math.round(key.factor * data[name] * decimals)/decimals;
-                    if (obj.allViews[name]) {
-                        obj.allViews[name].textContent = Math.floor(keyData) + 
-                                                          key.unit + "." +
-                                                          padToOrder(Math.floor(decimals * (keyData - Math.floor(keyData))), key.decimalsNum);
+                    for (var i = 0; i < obj.allKeys.length; i++) {
+                        var key = obj.allKeys[i];
+                        var decimals = Math.pow(10, key.decimalsNum);
+                        var name = key.name;
+                        var keyData = Math.round(key.factor * data[name] * decimals) / decimals;
+                        if (obj.allViews[name]) {
+                            obj.allViews[name].textContent = Math.floor(keyData) +
+                                                              key.unit + "." +
+                                                              padToOrder(Math.floor(decimals * (keyData - Math.floor(keyData))), key.decimalsNum);
+                        }
                     }
-                }   
 
-            });
+                });
 
-            var onKeyAdded = function (key, dom) {
-                obj.allKeys.push (key);
-                obj.allViews [key.name] = dom;
+                var onKeyAdded = function (key, dom) {
+                    obj.allKeys.push(key);
+                    obj.allViews[key.name] = dom;
+                }
+                var scrollableDiv = CreateDom(doms['div'], "div");
+                scrollableDiv.classList.add("scrollableRT");
+                CreateRtDomForPage(scrollableDiv, pageName, onKeyAdded);
             }
-            var scrollableDiv = CreateDom (doms['div'], "div");
-            scrollableDiv.classList.add ("scrollableRT");
-            CreateRtDomForPage (scrollableDiv, pageName, onKeyAdded);
         }
 
-        this.reset();
-    }
+        returnedViewer.reset();
+        return returnedViewer;
+    },
 
-    function alertDivTitle() {
-        alert (this.title);
-    }
+    alertDivTitle : function() { 
+        alert(this.title); 
+    },
 
-    function CreateLinkDom (pageName) {
+    CreateLinkDom : function(pageName) {
         var host = document.getElementById("rightNowFrontPage");
-        
+
         var createdDoms = {};
 
         var div = CreateDom(host, "div");
@@ -85,7 +91,7 @@ with this program. If not, see <https://www.gnu.org/licenses/agpl.html>. */
 
         createdDoms['div'] = div;
 
-        div.classList.add ("rightNowFrontPageWidget");
+        div.classList.add("rightNowFrontPageWidget");
 
         // TODO: the background class.
         // TODO: this should be from the page object.
@@ -95,123 +101,125 @@ with this program. If not, see <https://www.gnu.org/licenses/agpl.html>. */
 
         div.classList.add(realTimeBackgroundClassName);
 
-        createdDoms['a'] = CreateDom (div, "a");
+        createdDoms['a'] = CreateDom(div, "a");
         createdDoms['a'].setAttribute("href", "#" + pageName);
 
 
         var span = CreateDom(createdDoms['a'], "span", objectName);
         span.classList.add("realtimeTitle");
         return createdDoms;
-    };
+    }
 
-    function CreateRtDomForPage (domHost, pageName, onViewAdded) {
-        // same host
-        try {
-            // This wil throw initially. Notifications will not update the view.
-            for (var key in Pages[pageName].dataSource.getDataAsObjectForJD(0, false)) {
-                var createdDom = CreateDom(domHost, "div", "loading ...");
-                createdDom.classList.add(key);
+};
 
-                var unit = "\u00B0";
-                var scaleFactor = 1;
-                for (var tableKey in Pages[pageName].tableHeaderInfo) {
-                    if (key == Pages[pageName].tableHeaderInfo[tableKey].dataKey) {
-                        unit = Pages[pageName].tableHeaderInfo[tableKey]["1"]["text"];
-                        createdDom.setAttribute("alt", Pages[pageName].tableHeaderInfo[tableKey]["longText"]);
-                        createdDom.setAttribute("title", Pages[pageName].tableHeaderInfo[tableKey]["longText"]);
-                        createdDom.onclick = alertDivTitle;
-                    }
+function CreateRtDomForPage(domHost, pageName, onViewAdded) {
+    // same host
+    try {
+        // This wil throw initially. Notifications will not update the view.
+        for (var key in Pages[pageName].dataSource.getDataAsObjectForJD(0, false)) {
+            var createdDom = CreateDom(domHost, "div", "loading ...");
+            createdDom.classList.add(key);
+
+            var unit = "\u00B0";
+            var scaleFactor = 1;
+            for (var tableKey in Pages[pageName].tableHeaderInfo) {
+                if (key == Pages[pageName].tableHeaderInfo[tableKey].dataKey) {
+                    unit = Pages[pageName].tableHeaderInfo[tableKey]["1"]["text"];
+                    createdDom.setAttribute("alt", Pages[pageName].tableHeaderInfo[tableKey]["longText"]);
+                    createdDom.setAttribute("title", Pages[pageName].tableHeaderInfo[tableKey]["longText"]);
+                    createdDom.onclick = RealTimeDataViewer.alertDivTitle;
                 }
-                
-                if (unit == "'") {
-                    scaleFactor = 60;
-                } else if (unit == "''") {
-                    scaleFactor = 3600;
-                }else if (unit == "hh:mm") {
-                    unit = "h";
-                }
-
-                var decimalsNum = GetNumberOfDecimals (pageName, key);
-                if (IsVisible(pageName, key)) {
-                    createdDom.classList.remove ("hidden");
-                } else {
-                    createdDom.classList.add ("hidden");
-                }
-
-                onViewAdded ({"name" : key, "unit": unit, "decimalsNum" : 3, "factor" : scaleFactor}, createdDom);
             }
 
-        } catch (err) {
-            setTimeout (function() { CreateRtDomForPage(domHost, pageName, onViewAdded); }, 100);
+            if (unit == "'") {
+                scaleFactor = 60;
+            } else if (unit == "''") {
+                scaleFactor = 3600;
+            } else if (unit == "hh:mm") {
+                unit = "h";
+            }
+
+            var decimalsNum = GetNumberOfDecimals(pageName, key);
+            if (IsVisible(pageName, key)) {
+                createdDom.classList.remove("hidden");
+            } else {
+                createdDom.classList.add("hidden");
+            }
+
+            onViewAdded({ "name": key, "unit": unit, "decimalsNum": 3, "factor": scaleFactor }, createdDom);
         }
+
+    } catch (err) {
+        setTimeout(function () { CreateRtDomForPage(domHost, pageName, onViewAdded); }, 100);
+    }
+}
+
+function GetNumberOfDecimals(pageName, key) {
+    var numOfDecimals = localStorage.getItem(GetRTStorageKey("numOfDecimals", pageName, key));
+    if (numOfDecimals === null) {
+        numOfDecimals = 3;
+        localStorage.setItem(GetRTStorageKey("numOfDecimals", pageName, key), numOfDecimals);
     }
 
-    function GetNumberOfDecimals (pageName, key) {
-        var numOfDecimals = localStorage.getItem (GetRTStorageKey("numOfDecimals", pageName, key));
-        if (numOfDecimals === null) {
-            numOfDecimals = 3;
-            localStorage.setItem (GetRTStorageKey("numOfDecimals", pageName, key), numOfDecimals);
-        }
+    return numOfDecimals * 1.0;
+}
 
-        return numOfDecimals * 1.0;
-    }
-    
-    function IsVisible(pageName, key) {
+function IsVisible(pageName, key) {
 
-        var visible = localStorage.getItem (GetRTStorageKey("visible", pageName, key));
-        if (visible === null) {
+    var visible = localStorage.getItem(GetRTStorageKey("visible", pageName, key));
+    if (visible === null) {
 
         visible = true;
         if (pageName == "Jupiter Ephemeris") {
-            if (key == "CentralMeridianGeometricLongitude_System1" || 
-                key == "CentralMeridianGeometricLongitude_System2" ) {
-                    visible = false;
-                }
-        }
-        
-        if (pageName == "Moon Ephemeris") {
-            if (key == "RA" || key == "Dec" ) {
+            if (key == "CentralMeridianGeometricLongitude_System1" ||
+                key == "CentralMeridianGeometricLongitude_System2") {
                 visible = false;
             }
         }
 
-            localStorage.setItem (GetRTStorageKey("visible", pageName, key), visible);
+        if (pageName == "Moon Ephemeris") {
+            if (key == "RA" || key == "Dec") {
+                visible = false;
+            }
         }
 
-        return ('true' == visible);
+        localStorage.setItem(GetRTStorageKey("visible", pageName, key), visible);
     }
 
-    function GetRTStorageKey (purpose, pageName, key) {
-        return pageName + "/" + key + "/" + purpose;
+    return ('true' == visible);
+}
+
+function GetRTStorageKey(purpose, pageName, key) {
+    return pageName + "/" + key + "/" + purpose;
+}
+
+
+function CreateDom(parent, type, content) {
+    var child = parent.ownerDocument.createElement(type);
+    parent.appendChild(child);
+    if (content)
+        child.textContent = content;
+    return child;
+}
+
+function padToOrder(number, order) {
+    if (order < 2) {
+        return "" + number;
     }
 
+    var absNum = Math.abs(number);
+    var threshold = Math.pow(10, Math.floor(order - 1));
+    var res = "";
 
-    function CreateDom (parent, type, content) {
-        var child = parent.ownerDocument.createElement(type);
-        parent.appendChild(child);
-        if (content)
-            child.textContent =  content;
-        return child;
+    for (var i = 1; (i < order) && (absNum < threshold) ; i++, threshold *= 0.1) {
+        res += "0";
     }
-
-    function padToOrder (number, order) {
-        if (order < 2) {
-            return "" + number;
-        }
-
-        var absNum = Math.abs(number);
-        var threshold = Math.pow(10, Math.floor(order - 1));
-        var res = "";
-
-        for (var i = 1; (i < order) && (absNum < threshold); i++, threshold *= 0.1) {
-            res += "0";
-        }
-        if (number < 0) {
-            res = "-" + res;
-        }
-        return res + "" + absNum;
+    if (number < 0) {
+        res = "-" + res;
     }
-    
-    function padToTens (a) {
-        return padToOrder(a, 2);
-    }  
+    return res + "" + absNum;
+}
+
+function padToTens(a) {
+    return padToOrder(a, 2);
+}
