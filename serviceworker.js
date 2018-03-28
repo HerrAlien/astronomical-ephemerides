72deae28a005b2a8a5b675e4ffbe79e5def02397
Herr_Alien <garone80@gmail.com>
2018-03-28 18:08:51 +0300

    will need some cleanup ...

--- serviceworker.js
+++ serviceworker.js
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Affero General Public License along
 with this program. If not, see <https://www.gnu.org/licenses/agpl.html>. */
 (function() {
 var CACHE_PREFIX = 'Cache-for-ephemerides';
-var CACHE_VERSION = '147';
+var CACHE_VERSION = '148';
 var CACHE_NAME = CACHE_PREFIX + '-' + CACHE_VERSION;
 
 var optionalUrlsToCache = [
