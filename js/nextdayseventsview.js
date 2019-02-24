3e01e85b74753c5b0501a98712d35a88f1ff5508
Herr_Alien <devnull@localhost>
2019-02-24 12:22:49 +0200

    prepar a view

---
+++ js/nextdayseventsview.js
@@ -0,0 +1,35 @@
+/* ephemeris - a software astronomical almanach 
+
+Copyright 2017 Herr_Alien <alexandru.garofide@gmail.com>
+
+This program is free software: you can redistribute it and/or modify it under 
+the terms of the GNU Affero General Public License as published by the 
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/agpl.html>. */
+
+"use strict";
+
+/* Requirement for data sources:
+    // IncrementHint is just a hint on the sample rate for getting from the inernal data sources.
+    // If there is reasonable evidence to suspect an event, a finer icrement should be used.
+    GetEvents (countOfDays) 
+    -> [ { start: <JD>, end: <JD>, title: string, target : [actions]},
+         ... ... ...
+     ]
+
+   Data sources for next events:
+    - the moon and sun eclipses
+    - occultations
+*/
+
+
+(function(){
+
+})();
